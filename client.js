3162bf859935b2da3d547e7227aaf84391be3465
Hans Martin Bjørkevoll <72259074+hmbjorkevoll@users.noreply.github.com>
2021-09-20 10:19:38 +0200

    updated readme

--- client.js
+++ client.js
@@ -1,8 +1,8 @@
 import sanityClient from "@sanity/client";
 
 export default sanityClient({
-  projectId: "rnyt3x3f", // you can find this in sanity.json
-  dataset: "production", // or the name you chose in step 1
-  useCdn: true, // `false` if you want to ensure fresh data
+  projectId: "NEXT_PUBLIC_SANITY_PROJECT_ID", 
+  dataset: "NEXT_PUBLIC_SANITY_DATASET", 
+  useCdn: true,
   apiVersion: "2021-09-10",
 });
